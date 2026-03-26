e8d0c22de1c015f3764a07b43db6af8fe7c65542
Yohan-Ralambo <yohanralambo@gmail.com>
2026-03-26 10:03:52 +0300

    TD3

--- src/main/java/org/example/school/SchoolCOntroller.java
+++ src/main/java/org/example/school/SchoolCOntroller.java
@@ -1,49 +1,64 @@
 package org.example.school;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SchoolCOntroller {
 
     @GetMapping("/welcome")
-    public String Welcome(@RequestParam String name) {
-        return "Welcome " + name;
+    public ResponseEntity<String> welcome(@RequestParam String name) {
+       if (name == null) {
+           return ResponseEntity
+           .status(400)
+                   .body("Name is required");
+
+       }
+        return ResponseEntity
+        .status(HttpStatus.OK)
+        .body("Welcome " + name);
     }
 
     private List<Student> students = new ArrayList<>();
 
     @PostMapping("/students")
-    public String addStudents(@RequestBody List<Student> newStudents) {
-
-        students.addAll(newStudents);
-
-        return students.stream()
-                .map(s -> s.getFirstName() + " " + s.getLastName())
-                .collect(Collectors.joining(", "));
+    public ResponseEntity<?> addStudent(@RequestBody Student student) {
+        try {
+            students.add(student);
+            return ResponseEntity
+            .status(HttpStatus.CREATED)
+                .body(student);
+        } catch (Exception e){
+            return ResponseEntity
+                    .status(500)
+                    .body("Erreur interne du serveur");
     }
+}
 
     @GetMapping("/students")
     public ResponseEntity<?> getStudents(@RequestHeader("Accept") String accept) {
 
         if (!accept.contains("application/json") && !accept.contains("text/plain")) {
             return ResponseEntity
-                    .status(406)
-                    .body("Format non supporté");
+                    .status(501)
+                    .body("Erreur interne du serveur");
         }
 
-        if (accept.contains("text/plain")) {
-            String result = students.stream()
-                    .map(s -> s.getFirstName() + " " + s.getLastName())
-                    .collect(Collectors.joining(", "));
-            return ResponseEntity.ok().body(result);
+        try {
+            accept.equals("text/plain");
+            accept.equals("application/json");
+            return ResponseEntity
+                    .status(200)
+                    .body(students);
+        } catch (Exception e) {
+            return ResponseEntity
+            .status(500)
+            .body("Erreur interne du serveur");
         }
-
-        return ResponseEntity.ok().body(students);
     }
 
 }
